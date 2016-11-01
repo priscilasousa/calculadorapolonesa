328773fa2a5f0381ca9407765dfe9a8941632515
Priscila de Sousa Silva <cila_007@hotmail.com>
2016-10-31 21:37:24 -0300

    adc funcao main

--- ex17.c
+++ ex17.c
@@ -6,3 +6,13 @@ typedef struct st_pilha
     struct st_pilha *prox;
 
 }pilha;
+
+int main(void)
+{
+    pilha *CABECA = malloc(sizeof(pilha));
+    CABECA->prox = NULL;
+    printf("\nBem vindo a Calculadora Polonesa!\n");
+    return 0;
+}
+
+
