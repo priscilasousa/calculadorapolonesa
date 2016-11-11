6f922007cda2de8725a96ae5cf78fc88b61a0584
Gabriel de Brito Sa Magalhaes <gabrieldbsm@gmail.com>
2016-11-11 12:36:18 -0300

    declarando funcao e condicao

--- ex17.c
+++ ex17.c
@@ -6,12 +6,17 @@ typedef struct st_pilha
     struct st_pilha *prox;
 
 }pilha;
+int menu(pilha *CABECA);
 
 int main(void)
 {
+    int x = 0;
     pilha *CABECA = malloc(sizeof(pilha));
     CABECA->prox = NULL;
     printf("\nBem vindo a Calculadora Polonesa!\n");
+    while(x == 0){
+        x = menu(CABECA);
+    }
     return 0;
 }
 
